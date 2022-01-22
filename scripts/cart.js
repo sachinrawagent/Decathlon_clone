35ac4ab2bfe3962f65741b8cea3457dac200bcfa
Vaibhav Upasani <upasanivaibhav7@gmail.com>
2022-01-22 12:06:00 +0530

    complete final cart

--- scripts/cart.js
+++ scripts/cart.js
@@ -1,6 +1,6 @@
 let cart_data = JSON.parse(localStorage.getItem('cart_data'));
 // console.log("In the cart page", cart_data);
-let tp=0;
+let tp = 0;
 showProd(cart_data);
 showOrder(cart_data);
 function showProd(data, index) {
@@ -47,39 +47,48 @@ function showOrder(data) {
         price = price.split("");
         price.splice(0, 2);
         price = price.join("")
-        price=Number(price);
-        let main_div=document.createElement('div');//This is main div of the order
-        let order_text=document.createElement('h3');//This is for the show the text 
-        order_text.textContent="Order Summary";
-        let price_div=document.createElement('div');//This div is for the total price showing
-        price_div.id="vai_totprice";
-        let price_text=document.createElement('p');
-        price_text.textContent="Total Product (Inc GST)";
-        let total_price=document.createElement('p');
-        tp=tp+price;
-        total_price.textContent=`₹${tp}`;
-        price_div.append(price_text,total_price);
-        //This div is for the delivery charges
-        let del_chargediv=document.createElement('div');
-        del_chargediv.id="vai_delChargediv";
-        let del_text=document.createElement('p');
-        del_text.textContent="Estimate delivery fee";
-        let del_charge=document.createElement('p');
-        del_charge.textContent='₹129'
-        del_chargediv.append(del_text,del_charge);
-        let line=document.createElement('hr');
-        //This is for the last total price
-        let last_tpicediv=document.createElement('div');
-        last_tpicediv.id="vai_lastTotdiv";
-        let total=document.createElement('h3');
-        total.textContent="Total";
-        let last_total=document.createElement('p');
-        last_total.textContent=`${tp+129}`
-        main_div.append(order_text,price_div,del_chargediv,line);
-        document.querySelector('#vai_payment').append(main_div);
-
-
+        price = Number(price);
+        tp = tp + price;
     })
+    if (tp != 0) {
 
-
+        let main_div = document.createElement('div');//This is main div of the order
+        let order_text = document.createElement('h3');//This is for the show the text 
+        order_text.textContent = "Order Summary";
+        let price_div = document.createElement('div');//This div is for the total price showing
+        price_div.id = "vai_totprice";
+        let price_text = document.createElement('p');
+        price_text.textContent = "Total Product (Inc GST)";
+        let total_price = document.createElement('p');
+        // tp = tp + price;
+        total_price.textContent = `₹${tp}`;
+        price_div.append(price_text, total_price);
+        //This div is for the delivery charges
+        let del_chargediv = document.createElement('div');
+        del_chargediv.id = "vai_delChargediv";
+        let del_text = document.createElement('p');
+        del_text.textContent = "Estimate delivery fee";
+        let del_charge = document.createElement('p');
+        del_charge.textContent = '₹129'
+        del_chargediv.append(del_text, del_charge);
+        let line = document.createElement('hr');
+        //This is for the last total price
+        let last_tpicediv = document.createElement('div');
+        last_tpicediv.id = "vai_lastTotdiv";
+        let total = document.createElement('h3');
+        total.textContent = "Total";
+        let last_total = document.createElement('h3');
+        last_total.textContent = `${tp + 129}`;
+        last_tpicediv.append(total, last_total);
+        //proceed to checkout button
+        let proc_btn = document.createElement('button');
+        proc_btn.onclick = () => {
+            window.location.href = "payment.html";
+        }
+        proc_btn.id = "vai_procbtn";
+        proc_btn.textContent = "PROCEED TO CHECKOUT";
+        main_div.append(order_text, price_div, del_chargediv, line, last_tpicediv, proc_btn);
+        document.querySelector('#vai_payment').append(main_div);
+        console.log(tp, "Total price is");
+    }
 }
